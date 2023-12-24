3d6e21eb7d12fec28bf019abcd99136766ff3dc7
interrrp <int@addictedto.coffee>
2023-12-24 13:07:07 +0800

    Fix packaging script not copying assets

--- scripts/package.py
+++ scripts/package.py
@@ -24,12 +24,19 @@ def main() -> None:
         zip_name = f"VanillaPvP {version}.zip"
         zip_path = PACKAGE_DIR / zip_name
         with ZipFile(zip_path, "w") as zip_file:
-            for file in version_dir.glob("*"):
-                zip_file.write(file, file.name)
+            copytree_to_zip(version_dir, zip_file)
 
         print(f"{version} -> {zip_path}")
 
     print("Done!")
+
+
+def copytree_to_zip(source: Path, destination_zip: ZipFile):
+    with destination_zip as zipf:
+        for file_path in source.glob("**/*"):
+            if file_path.is_file():
+                archive_path = file_path.relative_to(source)
+                zipf.write(file_path, arcname=archive_path)
 
 
 if __name__ == "__main__":
